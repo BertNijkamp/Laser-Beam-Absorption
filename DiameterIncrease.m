6af0ada00a1c15abcf40f591ba00f79442c5b838
BertNijkamp <118302601+BertNijkamp@users.noreply.github.com>
2022-11-16 22:17:31 +0100

    Add files via upload

--- DiameterIncrease.m
+++ DiameterIncrease.m
@@ -1,11 +1,11 @@
 function [diamOut] = DiameterIncrease(diam,D)
-% This function calculates the increase in particle diameter due to two particles overlapping.
-% The particles need to have equal diameters.
+% This function calculates the increase in particle diameter due to two particles overlapping
+% Warning: particles need to have equal diameters
 
 if D < diam
     Drel = D/diam;
-    A = (Drel^3+64+8*sqrt(2*Drel^3+64))^(1/3);
-    factor = (Drel^2/A-Drel+A)/4;
+    A = (Drel^3+8+4*sqrt(Drel^3+4))^(1/3);
+    factor = (Drel^2/A-Drel+A)/2;
     diamOut = diam*factor;
 else
     diamOut = diam;
