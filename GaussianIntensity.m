bf680be676d080c7133c830e11257699d154c62f
BertNijkamp <118302601+BertNijkamp@users.noreply.github.com>
2022-11-20 16:09:09 +0100

    Update GaussianIntensity.m

--- GaussianIntensity.m
+++ GaussianIntensity.m
@@ -1,10 +1,10 @@
 function [I] = GaussianIntensity(Coords,centre,SpotRadius)
-% This function calculates the intensity for all rays in a circular pattern. The total intensity is 1.
+% This function calculates the starting energy for all rays in a circular pattern. The total energy is 1.
 
 % Distance from every point to the centre
 d = arrayfun(@(n)norm([Coords(n,1)-centre(1) Coords(n,2)-centre(2)]),(1:size(Coords,1))');
 
-% Intensity per ray
+% Energy per ray
 I = exp(-2*d.^2/SpotRadius^2)*2/(size(Coords,1)*(1-exp(-2)));
 I = I/sum(I);
 
